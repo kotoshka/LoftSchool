082d96a69a1633b26e3b6df8eab95a7a150f6ce2
kopose <koposAnnaPHP@gmail.com>
2018-06-02 21:46:46 +0300

    'test'

--- dz0.php
+++ dz0.php
@@ -5,13 +5,11 @@
  * Date: 02.06.2018
  * Time: 19:06
  */
+require_once "dz0_func.php";
 
 const TESTCONSTANTA = 'asd';
 $user_name = "Igor";
-function show_something()
-{
-    return 'SOMETHING';
-}
+
 if (1==1) {
     echo 'hi';
 }
---
+++ dz0_func.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Сабзира
+ * Date: 02.06.2018
+ * Time: 21:34
+ */
+
+function show_something()
+{
+    return 'SOMETHING';
+}
