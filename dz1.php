563320d3f4bbe77a07774343aa90bad1824ad8a1
kopose <koposAnnaPHP@gmail.com>
2018-06-02 19:16:09 +0300

    'first'

---
+++ dz1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Сабзира
+ * Date: 02.06.2018
+ * Time: 19:06
+ */
+
+const TESTCONSTANTA = 'asd';
+$user_name = "Igor";
+function show_something()
+{
+    return 'SOMETHING';
+}
+if (1==1) {
+    echo 'hi';
+}
