6ff632702ae575cc860869f1c8b035d53fe37b0c
kopose <koposAnnaPHP@gmail.com>
2018-06-07 00:58:23 +0300

    '5'

---
+++ dz2/index.php
@@ -0,0 +1,42 @@
+<?php
+require('src/functions.php');
+
+//task1
+$array = ['i am a string', 'i am an another string', 'i am a third string'];
+task1($array);
+$string = task1($array, true);
+echo nl2br($string . PHP_EOL);
+echo '<hr><br>';
+
+//task2
+$res = task2('+', 2, 4, 5, 6, 7.5);
+echo nl2br($res . PHP_EOL);
+/* вариации со знаком
+$res = task2('-', 2, 4, 5, 6, 7.5);
+echo nl2br($res . PHP_EOL);
+$res = task2('*', 2, 4, 5, 6, 7.5);
+echo nl2br($res . PHP_EOL);
+$res = task2('/', 2, 4, 5, 6, 7.5);
+echo nl2br($res . PHP_EOL);
+$res = task2('not a sign', 2, 4, 5, 6, 7.5);
+echo nl2br($res . PHP_EOL);*/
+/* вариации с числами
+$res = task2('+', '6', 4, 5, 6, 7.5);
+echo nl2br($res . PHP_EOL);
+*/
+echo '<hr><br>';
+
+//task3
+task3(4, 7);
+//task3('5', 7);
+echo '<hr><br>';
+
+//task4
+task4('d.m.Y H:i');
+task5('24.02.2016 00:00:00');
+echo '<hr><br>';
+
+//task5
+task6('Карл у Клары украл Кораллы');
+task7('Две бутылки лимонада');
+echo '<hr><br>';
---
+++ dz2/src/functions.php
@@ -0,0 +1,120 @@
+<?php
+
+function task1($array, $bool = false)
+{
+    if ($bool === true) {
+        $str = implode('; ', $array);
+        return $str;
+    } else {
+        foreach ($array as $string) {
+            echo '<p>' . $string . '</p>';
+        }
+    }
+}
+
+function task2(string $sign, ...$args)
+{
+    $arSigns = ['+', '-', '*', '/'];
+    if (!in_array($sign, $arSigns)) {
+        return "Неверно указан знак арифметического действия!";
+    } else {
+        if (!empty($args)) {
+            $result = '';
+            foreach ($args as $key => $arg) {
+                if (is_int($arg) || is_float($arg)) {
+                    if ($key != count($args) -1) {
+                        $result .= $arg . $sign;
+                    } else {
+                        $result .= $arg . ' = ';
+                    }
+                    if ($key === 0) {
+                        $count = $arg;
+                    } else {
+                        switch ($sign) {
+                            case '+':
+                                $count = $count + $arg;
+                                break;
+                            case '-':
+                                $count = $count - $arg;
+                                break;
+                            case '*':
+                                $count = $count * $arg;
+                                break;
+                            case '/':
+                                $count = $count / $arg;
+                                break;
+                        }
+                    }
+                } else {
+                    return 'Не все аргументы, переданные в функцию, являются числом или дробным числом!';
+                }
+            }
+            $result .= $count;
+        }
+        return $result;
+    }
+}
+
+//function task3(int $a, int $b) - так тоже можно,
+//но int вначале преобразует в целое число все, что может ('5' , 5.5 => 5)
+function task3($a, $b)
+{
+    if (is_int($a) && is_int($b)) {
+        $table = '<table style="border-collapse:collapse;">';
+        for ($tr=0; $tr<=$a; $tr++) {
+            $table .= '<tr>';
+            if ($tr === 0) {
+                for ($td=0; $td<=$b; $td++) {
+                    if ($td === 0) {
+                        $table .= '<td style="border:1px solid black;padding:25px;"></td>';
+                    } else {
+                        $table .= '<td style="border:1px solid black;padding:25px;">' . $td . '</td>';
+                    }
+                }
+            } else {
+                for ($td=0; $td<=$b; $td++) {
+                    if ($td === 0) {
+                        $table .= '<td style="border:1px solid black;padding:25px;">' . $tr . '</td>';
+                    } else {
+                        $table .= '<td style="border:1px solid black;padding:25px;">' . $td * $tr . '</td>';
+                    }
+                }
+            }
+            $table .= '</tr>';
+        }
+        $table .= '</table>';
+        echo $table;
+    } else {
+        echo nl2br('Оба параметра, переданные в функцию, должны быть целым числом!' . PHP_EOL);
+    }
+}
+
+function task4($format)
+{
+    echo nl2br(date($format) . PHP_EOL);
+}
+
+function task5(string $date)
+{
+    echo nl2br(strtotime($date) . PHP_EOL);
+}
+
+function task6(string $str)
+{
+    $newStr = str_replace('К', '', $str);
+    echo nl2br($newStr . PHP_EOL);
+}
+
+function task7(string $str)
+{
+    $newStr = str_replace('Две', 'Три', $str);
+    echo nl2br($newStr . PHP_EOL);
+}
+
+function task8(string $date)
+{
+}
+
+function task9(string $date)
+{
+}
